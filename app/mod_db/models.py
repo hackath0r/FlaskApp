f670734b3262ce2704191cff7378e496aa00e5c7
Prashant <prashant.bhrgv@gmail.com>
2017-09-21 15:59:21 +0530

    added methods to inspect db models

--- app/mod_db/models.py
+++ app/mod_db/models.py
@@ -15,6 +15,22 @@ class BaseModel(db.Model):
     def __init__(self, *args):
         super().__init__(*args)
 
+    def __repr__(self):
+        """Define a base way to print models"""
+        return '%s(%s)' % (self.__class__.__name__, {
+            column: value
+            for column, value in self._to_dict().items()
+        })
+
+    def json(self):
+        """
+        Define a base way to jsonify models, dealing with datetime objects
+        """
+        return {
+            column: value if not isinstance(value, datetime.date) else value.strftime('%Y-%m-%d')
+            for column, value in self._to_dict().items()
+        }
+
 #class YourModel(BaseModel):
 #"""model for one of your table"""
 #    __tablename__ = 'my_table'
