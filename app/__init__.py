3b0f07513a10d5b7baa4b08bef36e5e2c73203d6
Prashant <prashant.bhrgv@gmail.com>
2017-09-22 02:42:38 +0530

    registering common routes blueprint in app init

--- app/__init__.py
+++ app/__init__.py
@@ -13,18 +13,15 @@ app.config.from_object('config')
 # Import db module from mod_db
 from app.mod_db.models import db
 
+# Import routes blueprint
+from routes import routes
+
+# Register blueprint(s)
+app.register_blueprint(routes)
+
 # Connect SQLAlchemy object to application
 db.init_app(app)
 db.app = app
-
-# Handle HTTP error
-@app.errorhandler(404)
-def not_found(erro):
-    return render_template('404.html'), 404
-
-@app.route('/')
-def index():
-    return  render_template('index.html')
 
 # Build the database:
 # This will create the database file using SQLAlchemy
